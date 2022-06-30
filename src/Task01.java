81d615baca83d6b6c0b36bba7e86548f297212ed
st <pomalejko.vova@gmail.com>
2022-06-30 20:04:12 +0300

    Initial commit

---
+++ src/Task01.java
@@ -0,0 +1,9 @@
+public class Task01 {
+    public static void main(String[] args) {
+        Student std1 = new Student();
+        System.out.println(std1.name);
+        System.out.println(std1.age);
+        System.out.println(std1.mark);
+        System.out.println(std1.alive);
+    }
+}
